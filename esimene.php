784b892c3b1c52cbd209af8ba88fe07c582a2aa4
Simone Niinemägi <niinsimo@users.noreply.github.com>
2017-09-12 14:15:00 +0300

    Lisatud on esimene jupp PHP koodi

---
+++ esimene.php
@@ -0,0 +1,16 @@
+<DOCTYPE html>
+<html>
+<head>
+  <meta charset-''utf-8''>
+  <title>Simone Niinemägi informaatika</title>
+  
+</head>
+<body>
+  <h1> Simone tervitab</h1>
+  <p> see ilmselt ei tööta aga ma proovin ikka
+nvm töötab ikka woopp</p>
+<?php
+ echo "See on esimene jupp PHP abil väljastatud teksti"
+?>
+
+</body> 
